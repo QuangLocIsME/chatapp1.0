e7015b6ea14b7fe7dd8bc7049e1dc9c7530a9109
Quang Loc <nguyenquangloc2004@gmail.com>
2024-11-16 10:53:51 +0700

    added login page 2 step
    Đã thêm page login page 2 step

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,10 +1,13 @@
 // lib/constants.js
 export const API_ROUTES = {
     REGISTER: '/api/auth/register',
-    LOGIN: '/api/auth/login',
+    CHECKMAIL: '/api/auth/check-email',
+    CHECKPASSWORD: '/api/auth/check-password',
 };
 
 export const MESSAGES = {
     REGISTRATION_SUCCESS: 'You have successfully registered. Please log in to continue.',
     REGISTRATION_FAILURE: 'An error occurred during registration.',
+    EMAIL_EXISTS: 'Email not available. Please try another email.',
+
 };
